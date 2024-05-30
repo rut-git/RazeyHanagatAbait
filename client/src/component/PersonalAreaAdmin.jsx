334a222887d2d19f854cbfb810e43c3c14e53df6
rut-git <r3208616@gmail.com>
2024-05-30 15:52:42 +0300

    update PersonalArea and videoList

--- client/src/component/PersonalAreaAdmin.jsx
+++ client/src/component/PersonalAreaAdmin.jsx
@@ -1,42 +1,4 @@
-// import React, { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios";
-// import {useGetDialogsQuery} from '../app/dialogApiSlice'
-// import { Button } from "primereact/button";
-// import AdminDialogue from './AdminDialogue'
 
-// const PersonalAreaAdmin = (props) => {
-//     const [show,setShow]=useState(false)
-//    const navigate=useNavigate()
-//     const Request = async () => {
-       
-//         const ans = await axios("http://localhost:1260/api/functionToken/" + localStorage.getItem("token"))
-        
-//         if (ans.data.ans == false) {
-
-//             navigate("/login")
-//         }
-//     }
-//     useEffect(() => {
-//          Request();
-        
-// }, [])
-// // const getAllUsers=()=>{
-    
-    
-    
-// // }
-
-
-//     return (
-
-//         <div>
-//             <Button onClick={()=>{setShow(true)}} className="pi pi-messages" style={{marginTop:'3%'}}>הצג הודעות</Button>
-//             {/* /<Button onClick={()=>{navigate('/register')}}className="pi pi-user-plus">הוסף משתמש</Button> */}
-//             {show&&<AdminDialogue />}
-//         </div>)
-// }
-// export default PersonalAreaAdmin
 
 import { useGetPathByIdQuery } from '../app/videoApiSlice';
 import React, { useEffect, useState } from "react";
--- client/src/component/VideoList.jsx
+++ client/src/component/VideoList.jsx
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react';
 import './Article.css';
 import { useGetAllVideosQuery, useDeleteVideoMutation, useGetVideoByRoleQuery } from '../app/videoApiSlice';
